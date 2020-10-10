b989dc37e6fa70bff358c896abea71a3f4984bdc
luiis <luiis@DESKTOP-IN8D4GQ>
2020-10-10 11:50:12 -0300

    Adicionando as duas outras faturas ao processador de pagamentos

--- TDD/test/fatura/processadorDeBoletoTest.java
+++ TDD/test/fatura/processadorDeBoletoTest.java
@@ -29,5 +29,24 @@ public class processadorDeBoletoTest {
 		assertEquals(fatura.isPago(), true);
 	}
 	
+	@Test
+	public void testFatura2() {
+		fatura = new Fatura("Luis", 1500);
+		pagamentos.add(new Pagamento(1, 1000, "BOLETO"));
+		pagamentos.add(new Pagamento(2, 500, "BOLETO"));
+		pagamentos.add(new Pagamento(3, 250, "BOLETO"));
+		fatura.addArrayPagamento(pagamentos);
+		assertEquals(fatura.getFaltaPagar(), 0);
+		assertEquals(fatura.isPago(), true);
+	}
+	
+	@Test
+	public void testFatura3() {
+		fatura = new Fatura("Sergio", 2000);
+		pagamentos.add(new Pagamento(1, 500, "BOLETO"));
+		pagamentos.add(new Pagamento(2, 400, "BOLETO"));
+		assertEquals(fatura.getFaltaPagar(), 2000);
+		assertEquals(fatura.isPago(), false);
+	}
 
 }
