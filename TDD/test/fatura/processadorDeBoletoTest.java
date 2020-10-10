4bd7c7f3dadd42b2a629a53142c001283d2d02a4
luiis <luiis@DESKTOP-IN8D4GQ>
2020-10-10 11:42:11 -0300

    Criando metodo de teste para adicionar array de pagamentos

--- TDD/test/fatura/processadorDeBoletoTest.java
+++ TDD/test/fatura/processadorDeBoletoTest.java
@@ -1,5 +1,32 @@
 package fatura;
 
-public class processadorDeBoleto {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pagamento.Pagamento;
+
+public class processadorDeBoletoTest {
+	private Fatura fatura;
+	private static ArrayList<Pagamento> pagamentos;
+	@BeforeEach
+	public static void inicializaTodos() {
+		pagamentos = new ArrayList<Pagamento>();
+	}
+	
+	@Test
+	public void testFatura1() {
+		fatura = new Fatura("Luis Sergio", 1500);
+		pagamentos.add(new Pagamento(1, 500, "BOLETO"));
+		pagamentos.add(new Pagamento(2, 400, "BOLETO"));
+		pagamentos.add(new Pagamento(3, 600, "BOLETO"));
+		fatura.addArrayPagamento(pagamentos);
+		assertEquals(fatura.getFaltaPagar(), 10);
+		assertEquals(fatura.isPago(), true);
+	}
+	
 
 }
