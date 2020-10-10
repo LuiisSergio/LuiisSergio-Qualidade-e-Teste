ebb39b5931c175adcf96c57618cdfbf412ea411a
luiis <luiis@DESKTOP-IN8D4GQ>
2020-10-10 11:27:08 -0300

    Adicionando um pagamento e verificando o restante da fatura

--- TDD/test/fatura/FaturaTest.java
+++ TDD/test/fatura/FaturaTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import pagamento.Pagamento;
+
 @DisplayName("Classe para teste da fatura")
 public class FaturaTest {
 	
@@ -14,5 +16,9 @@ public class FaturaTest {
 	public void testFatura1() {
 		fatura = new Fatura("Luis Sergio", 1500);
 		assertEquals(fatura.getNomeCliente(), "Luis Sergio");
+		Pagamento boleto1 = new Pagamento(1, 500, "BOLETO");
+		fatura.addPagamento(boleto1);
+		assertEquals(fatura.getFaltaPagar(), 1000);
+		
 	}
 }
